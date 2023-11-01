646154c0cf39640b6984d0828b9342ffe8de1c5a
Bruno Aziz Spring Machado <brunospring@ufpr.br>
2023-11-01 15:54:32 -0300

    printando o valor da heap

--- main.s
+++ main.s
@@ -1,15 +1,24 @@
-section .data
-    heap_start quad 0 
+.section .data
+    heapStart: .quad 0 
+    format:    .asciz "Valor de heapStart: %lld\n"
 
-section .text
-    global _start
-
-_start:
-    movq $12, %rax     # nao entendi muito bem ainda, mas 12 em rax é o código do brk
+.section .text
+.global main
+main:
+    movq $12, %rax     # 12 em rax é o código do brk
     movq $0, %rdi  # nao faz nada na heap, só indica o endereço atual
     syscall
 
-    movq %rax, [heap_start] 
+    movq %rax, %rcx
 
+    movq $format, %rdi  # Configura o primeiro argumento para printf (formato da string)
+    movq %rcx, %rsi
+    call printf
+    
     movq $60, %rax         
     syscall
+
+
+# Rodar com:
+# as main.s -o malloc.o   
+# ld malloc.o -o imprime -dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc -e main
