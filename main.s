bc82b0af0dbe7abe366edcccbbc37a0cce55dbb5
Bruno Aziz Spring Machado <brunospring@ufpr.br>
2023-11-01 15:57:24 -0300

    printando o valor da heap

--- main.s
+++ main.s
@@ -1,3 +1,7 @@
+# Rodar com:
+# as main.s -o malloc.o   
+# ld malloc.o -o imprime -dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc -e main
+
 .section .data
     heapStart: .quad 0 
     format:    .asciz "Valor de heapStart: %lld\n"
@@ -9,16 +13,11 @@ main:
     movq $0, %rdi  # nao faz nada na heap, só indica o endereço atual
     syscall
 
-    movq %rax, %rcx
+    movq %rax, heapStart
 
     movq $format, %rdi  # Configura o primeiro argumento para printf (formato da string)
-    movq %rcx, %rsi
+    movq heapStart, %rsi
     call printf
     
     movq $60, %rax         
     syscall
-
-
-# Rodar com:
-# as main.s -o malloc.o   
-# ld malloc.o -o imprime -dynamic-linker /lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 -lc -e main
