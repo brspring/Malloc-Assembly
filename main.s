82beb7e6c3c5979714eed1264308ee0183e1808e
Gustavo Paulino <gustavo.vinicius.paulino@gmail.com>
2023-11-14 20:34:06 -0300

    altera rcx por r12

--- main.s
+++ main.s
@@ -33,44 +33,44 @@ dismiss_brk:
 
 memory_alloc:
     movq %rdi, %rbx         # parametro no rbx
-    movq heapStart, %rcx    # heapStart no rcx, rcx = heapStart
+    movq heapStart, %r12    # heapStart no r12, r12 = heapStart
     movq $12, %rax          # rax esta com valor atual do brk
     movq $0, %rdi
     syscall
 
     _loop_start:
-        cmpq %rcx, %rax
+        cmpq %r12, %rax
         je _tudo_ocupado        # nao encontrou nenhum bloco livre, aloca no fim 
-        cmpq $1, (%rcx)         # compara o valor do offset com 1
-        addq $8, %rcx           # soma 8 no rcx pra ir pro tamanho do bloco
-        jne _livre              # soma 8 no rcx                          
-        addq (%rcx), %rcx       # soma o tamanho do bloco no rcx para ir para  o fim do bloco menos 8 bytes
-        addq $8, %rcx           # vai para o inicio do proximo bloco
+        cmpq $1, (%r12)         # compara o valor do offset com 1
+        addq $8, %r12           # soma 8 no r12 pra ir pro tamanho do bloco
+        jne _livre              # soma 8 no r12                          
+        addq (%r12), %r12       # soma o tamanho do bloco no r12 para ir para  o fim do bloco menos 8 bytes
+        addq $8, %r12           # vai para o inicio do proximo bloco
         jmp _loop_start
         _livre:
-            cmpq (%rcx), %rdi   # compara o tamanho do bloco atual com o tamanho pedido
+            cmpq (%r12), %rdi   # compara o tamanho do bloco atual com o tamanho pedido
             jg _proximo_bloco   # se o tamanho do bloco atual for menor que o tamanho pedido, vai para o proximo bloco
-            movq %rdi, (%rcx)   # coloca o tamanho novo no segundo quadradinho
-            subq $8, %rcx       # vai para o inicio do bloco 
-            movq $1, (%rcx)     # diz que ta ocupado
-            movq %rcx, %rax     
+            movq %rdi, (%r12)   # coloca o tamanho novo no segundo quadradinho
+            subq $8, %r12       # vai para o inicio do bloco 
+            movq $1, (%r12)     # diz que ta ocupado
+            movq %r12, %rax     
             addq $16, %rax      # retorna o começo do bloco  de dados
             ret
             _proximo_bloco:
-                addq (%rcx), %rcx   # vai para o proximo bloco
-                addq $8, %rcx       # vai para o proximo bloco
+                addq (%r12), %r12   # vai para o proximo bloco
+                addq $8, %r12       # vai para o proximo bloco
                 jmp _loop_start
         _tudo_ocupado:
-          movq %rcx, %rdi    # coloca em rdi o valor de rcx, que é o valor atual da heapstart
+          movq %r12, %rdi    # coloca em rdi o valor de r12, que é o valor atual da heapstart
           addq $16, %rdi     
           addq %rbx, %rdi # rdi recebe o valor novo do brk
           movq $12, %rax
           syscall
-          movq $1, (%rcx) # coloca 1 para falar que o novo bloco ta ocupado
-          addq $8, %rcx
-          movq %rbx, (%rcx) # coloca o tamanho do novo bloco
-          addq $8, %rcx # rcx tem o endereço do começo do bloco
-          movq %rcx, %rax # oo endereço do começo  do novo bloco é movido no rax p retornar
+          movq $1, (%r12) # coloca 1 para falar que o novo bloco ta ocupado
+          addq $8, %r12
+          movq %rbx, (%r12) # coloca o tamanho do novo bloco
+          addq $8, %r12 # r12 tem o endereço do começo do bloco
+          movq %r12, %rax # oo endereço do começo  do novo bloco é movido no rax p retornar
 
     # loop que procura a primeira os 8bytes que indicam uma posicao livre
     # se achar, verifica os segundos 8bytes para ver se tem espaco suficiente
--- testa.c
+++ testa.c
@@ -4,9 +4,11 @@
 
 int main() {
     setup_brk(); // Inicializa o heap
-
+    
     //printf("Valor de heapStart antes da alocação: %p\n", setup_brk());
-    printf("Valor de heapStart após a alocação: %p\n", memory_alloc(32));
+    //printf("Valor de heapStart após a alocação: %p\n", memory_alloc(32));
+    printf("%p", memory_alloc(32));
+    printf("SAH");
 
 //    dismiss_brk(); // Libera a memória alocada
 
